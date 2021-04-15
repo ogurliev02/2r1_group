21f52bee73760579d5192db2d22829f25279a980
Гамид Огурлиев <ogurliev02@MacBook-Pro-Gamid.local>
2021-04-15 10:28:58 +0300

    add 15.04

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 from flask_avatars import Avatars
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,5 +17,6 @@ login.login_view = 'login'
 avatars = Avatars(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors, forms
